39e3bb07310a0f35b67946bf882fa455926ed92b
Fabio Santiago <fabiosal.rj@gmail.com>
2024-08-26 23:13:21 -0300

    Inicio

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from perfil import models
 
-# Register your models here.
+class EnderecoInline(admin.TabularInline):
+    model = models.Endereco
+    extra = 1
+
+@admin.register(models.Perfil)
+class PedidoAdmin(admin.ModelAdmin):
+    inlines = [
+        EnderecoInline
+    ]
+
+@admin.register(models.Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    ...
